f81636f297e2dffe5a085deea26bc8c4d6216bce
Dmitry Sedelnikov <dmnorc@users.noreply.github.com>
2017-03-17 11:23:30 +0600

    Update UserProfile.py

--- okta/models/user/UserProfile.py
+++ okta/models/user/UserProfile.py
@@ -1,24 +1,23 @@
 class UserProfile:
-
     types = {
         'login': str,
         'email': str,
         'secondEmail': str,
         'firstName': str,
         'lastName': str,
-        'mobilePhone': str
+        'mobilePhone': str,
+        'userType': str,
+        'organization': str,
+        'nickName': str
     }
 
     def __init__(self):
-
         self.login = None  # str
-
         self.email = None  # str
-
         self.secondEmail = None  # str
-
         self.firstName = None  # str
-
         self.lastName = None  # str
-
         self.mobilePhone = None  # str
+        self.organization = None # str
+        self.userType = None # str
+        self.nickName = None # str
