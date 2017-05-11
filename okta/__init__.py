6cf93efdc21a41f0862663fd1dd7bbea3ef87078
dron888 <dron888@users.noreply.github.com>
2017-05-11 15:14:40 +0600

    Increased version.

--- okta/__init__.py
+++ okta/__init__.py
@@ -7,7 +7,7 @@
     :license: Apache 2, see LICENSE.txt for more details.
 """
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 from .AppInstanceClient import AppInstanceClient
 from .AuthClient import AuthClient
